8502fc0b5768923977ff311e77208b489fb5c341
santiago sierra <santiscano@gmail.com>
2023-04-07 06:40:57 -0500

    build: version 1 header, navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import "./App.css";
-// import Navbar from "./modules/Navbar";
-// import Headers from "./modules/Headers";
-// import Portfolio from "./modules/Portfolio";
-// import Navbar2 from "./modules/Navbar2";
+import Navbar from "./modules/Navbar";
+import Headers from "./modules/Headers";
+import Portfolio from "./modules/Portfolio";
+import Navbar2 from "./modules/Navbar2";
 import Header from "./modules/Header";
 import css from "./sass/app.module.scss";
 
@@ -11,19 +11,19 @@ function App() {
   return (
     <div className="app">
       {/* <Navbar /> */}
-      {/* <div className="app__header-navbar"> */}
-      {/* <Navbar2 /> */}
-      {/* <Headers /> */}
-      {/* </div> */}
+      <div className="app__header-navbar">
+        <Navbar2 />
+        <Headers />
+      </div>
       {/* <header className="aqui">hola mundo</header> */}
       {/* <Portfolio /> */}
       {/* <FeaturesBenefits/> */}
       {/* <Price/> */}
       {/* <FAQs/> */}
       {/* <Footer/> */}
-      <div className={`bg-primary ${css.container}`}>
+      {/* <div className={`bg-primary ${css.container}`}>
         <Header />
-      </div>
+      </div> */}
     </div>
   );
 }
